3d84237e51648c2501bd43f3394236900bd82c19
Lukichas <lucamoreira256@gmail.com>
2019-10-11 23:03:49 -0300

    Testando configurações do cors

--- src/main/java/br/com/eubusco/server/service/CidadeService.java
+++ src/main/java/br/com/eubusco/server/service/CidadeService.java
@@ -2,6 +2,7 @@ package br.com.eubusco.server.service;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import br.com.eubusco.server.model.Cidade;
 @RequestMapping("cidadeService")
 public interface CidadeService {
 
+	@CrossOrigin
 	@RequestMapping(path = "adquirirTodos", method = RequestMethod.GET, produces = "application/json")
 	public abstract List<Cidade> adquirirTodos();
 
--- src/main/java/br/com/eubusco/server/service/UsuarioService.java
+++ src/main/java/br/com/eubusco/server/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.eubusco.server.service;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import br.com.eubusco.server.dto.RetornoLoginDTO;
 @RequestMapping("usuarioService")
 public interface UsuarioService {
 
+	@CrossOrigin
 	@RequestMapping(path = "realizarLogin", method = RequestMethod.POST, produces = "application/json")
 	public abstract RetornoLoginDTO realizarLogin(LoginDTO loginDTO);
 
